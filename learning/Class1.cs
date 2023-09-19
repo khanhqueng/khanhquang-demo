69830fb964beb3c7a2e9c081d9f6ea9015095e7f
khánh quang <quangkhanh288123@gmail.com>
2023-09-19 11:10:23 +0700

    add learning

---
+++ learning/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning
+{
+    internal class Class1
+    {
+        
+        private int tu, mau;
+        Class1(){ tu = 0; mau = 1; }
+        public void nhap() { 
+            tu = Convert.ToInt32(Console.ReadLine());
+            mau = Convert.ToInt32(Console.ReadLine());                   
+        }
+        public static Class1 operator +(Class1 a, Class1 b)
+        {
+            Class1 c = new Class1();
+            c.tu = a.tu*b.mau + b.tu*a.mau;
+            c.mau = a.mau * b.mau;
+            return c;
+        }
+        
+            
+    }
+}
---
+++ learning/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+namespace learning
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
